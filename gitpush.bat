ea7939b034f31e847b41ca4de8b5664a9a35d852
rweil55 <github@shaw-weil.com>
2024-10-15 20:04:18 -0400

    from indesign 2024-10-16 00:03

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd /inetpub/wwwroot/w/indiancreek
 
 
 git add .
-git commit -m "from indesign 2024-10-15 13:06" --no-edit
+git commit -m "from indesign 2024-10-16 00:03" --no-edit
 git push https://github.com/rweil55/web-indiancreek main
 
 rem wait for one minute to allow git hub to get its act together
